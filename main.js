f2f9039b1cfd9f775b1436c7f876adc27334ec11
christian birego <93344052+birego@users.noreply.github.com>
2024-05-11 21:58:51 +0200

    ajout du type

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
 
+const fetchPageContent = async (url) => {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error('Erreur lors de la récupération de la page :', error);
+    throw error;
+  }
+};
+
+const stripHTML = (html) => {
+  const $ = cheerio.load(html);
+  return $('body').text(); // Obtenez simplement le texte du corps de la page
+};
+
+const main = async () => {
+  const url = 'https://github.com/birego';
+  try {
+    const html = await fetchPageContent(url);
+    const textWithoutHTML = stripHTML(html);
+    console.log(textWithoutHTML);
+  } catch (error) {
+    console.error('Une erreur s\'est produite :', error);
+  }
+};
+
+main();
+
